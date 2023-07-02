3296a7d2ede8066c7ebc42a17f614e60e1dbe24a
midnyt <theplashrz@gmail.com>
2023-07-02 18:54:00 +0530

    update 1

---
+++ src/main/java/pack1/LoginServlet.java
@@ -0,0 +1,35 @@
+package pack1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class LoginServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String name = request.getParameter("user");
+		String pass = request.getParameter("pass");
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		if(name.equals("user1")&&pass.equals("pass1")) {
+			pw.println("Welcome "+name);
+		}else {
+			pw.println("Invalid user");
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
