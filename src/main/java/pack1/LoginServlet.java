6d77b416b398f455f6a03abd3653f9d4d5121328
midnyt <theplashrz@gmail.com>
2023-07-08 13:44:58 +0530

    re update

--- src/main/java/pack1/LoginServlet.java
+++ src/main/java/pack1/LoginServlet.java
@@ -1,7 +1,7 @@
 package pack1;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +11,11 @@ import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
